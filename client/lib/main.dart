0293c7d2cd5b5b3fab76cf2369ada0c24a10e5b3
mrfauzii <reishifauzi24@gmail.com>
2025-12-01 15:34:32 +0700

    feat: Implement splash screen customization for Android and iOS
    - Added multiple drawable resources for Android splash screens across different densities (hdpi, mdpi, xhdpi, xxhdpi, xxxhdpi).
    - Updated launch_background.xml to include new splash and background images.
    - Created styles for Android 12+ to handle splash screens with custom themes.
    - Integrated flutter_native_splash package for enhanced splash screen management.
    - Modified main.dart to preserve splash screen during app initialization.
    - Updated iOS assets for launch images and background.
    - Enhanced web splash screen with responsive images for light and dark modes.
    - Updated pubspec.yaml to include new dependencies and asset configurations.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:client/route.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(Duration(seconds: 2));
+  FlutterNativeSplash.remove();
+
   runApp(const MyApp());
 }
 
